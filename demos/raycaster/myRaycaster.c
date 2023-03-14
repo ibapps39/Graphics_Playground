af1e5017e79ff27f386866bee3546e7cc04cc8a3
Ian Brown <ibapps39@gmail.com>
2023-03-13 22:17:10 -0600

    init raycaster struggle

--- demos/raycaster/myRaycaster.c
+++ demos/raycaster/myRaycaster.c
@@ -1,5 +1,64 @@
 #include "myRaycaster.h"
 
+void init_raycaster(GLFWwindow* window) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //view angle, aspect ratio, near and far clipping distance
+    gluPerspective(60.0f, ASPECT, .1f, 100.0f);
+    // sets up model (world) and view (camera) -> modelview matrix
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    Player player = {
+        .x          = 2.0f,
+        .y          = 2.0f,
+        .direction  = 0.0f,
+    };
+
+    glfwSetKeyCallback(window, handle_keys);
+    //ref ensures that the player is alive as long as the window vs seperately allocating and freeing
+    glfwSetWindowUserPointer(window, &player);
+}
+
 int main() {
-    return 0;
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(
+        SCREEN_WIDTH, 
+        SCREEN_HEIGHT, 
+        SCREEN_TITLE, 
+        NULL, 
+        NULL
+        );
+    
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    init_raycaster(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(0);
 }
--- demos/raycaster/myRaycaster.h
+++ demos/raycaster/myRaycaster.h
@@ -1,3 +1,60 @@
-#pragma once
-#include "SDL/SDL.h"
-#include "SDL2/SDL_opengl.h"
+#ifndef RAYCASTER_H
+#define RAYCASTER_H
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
+#ifdef __linux__
+#include <GL/glu.h>
+#else
+#include <GL/glu.h>
+#endif
+
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Define constants for screen width and height
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define SCREEN_TITLE "SCREEN_TITLE"
+#define ASPECT (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT
+
+#define mapX 10
+#define mapY 10
+// Define a struct to represent the player's position and direction
+typedef struct {
+    float x;
+    float y;
+    float direction;
+} Player;
+
+// Define a struct to represent a ray
+typedef struct {
+    float x;
+    float y;
+    float distance;
+    int wallHit;
+} Ray;
+
+// Define a function to initialize the raycaster
+void init_raycaster(GLFWwindow* window);
+
+// Define a function to handle key presses
+void handle_keys(GLFWwindow* window, int key, int code, int action, int mods);
+
+// Define a function to render the scene
+void render_scene(Player* player);
+
+// Define a function to calculate the distance to the nearest wall
+float cast_ray(Player* player, float angle);
+
+#endif
